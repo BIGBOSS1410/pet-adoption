6c87994e2f0aa4ff94a909756d8d9eaf97514559
Boss <nuttapon_kbr@hotmail.com>
2025-12-12 11:11:09 +0700

    Turned pet card into a JS template

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const template = document.querySelector('#pet-card-template')
+const warpper = document.createDocumentFragment()
+
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
@@ -14,8 +18,10 @@ async function petsArea() {
   const petPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petPromise.json()
   petsData.forEach((pet) => {
-    console.log(pet)
+    const clone = template.content.cloneNode(true)
+    warpper.appendChild(clone)
   });
+  document.querySelector('.list-of-pets').appendChild(warpper)
 }
 
 petsArea()
