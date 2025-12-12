5a6298294b5b829137a5883972418c33040ae020
Boss <nuttapon_kbr@hotmail.com>
2025-12-12 15:23:45 +0700

    Naerly finished pet card feature.

--- main.js
+++ main.js
@@ -19,9 +19,27 @@ async function petsArea() {
   const petsData = await petPromise.json()
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true)
+
+    clone.querySelector('h3').textContent = pet.name
+    clone.querySelector('.pet-description').textContent = pet.description
+    clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
+    clone.querySelector('.pet-card-photo img').src = pet.photo
+    clone.querySelector('.pet-card-photo img').alt = `A ${pet.species} named ${pet.name}`
+
     warpper.appendChild(clone)
   });
   document.querySelector('.list-of-pets').appendChild(warpper)
 }
 
 petsArea()
+
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
+
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a year old"
+
+  return `${age} year old`
+}
